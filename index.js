d951cbc3faebb15eef68d4e13a732d63b4c949b6
MatiasPericolo <matiaspericolo@gmail.com>
2023-06-25 12:36:53 -0300

    Cambio de index y preparación para vercel

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
 
+//const apiProductosController = require('./apiProductosController');
+const apiCategoriasController = require('./apiCategoriasController');
+const apiPedidosController = require('./apiPedidosController');
+const apiClientesController = require('./apiClientesController');
+
 app.use(express.json());
 
-const { createClient } = require('@supabase/supabase-js');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
 
-const supabaseUrl = 'https://bltnpoukibscmpjgvdtc.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsdG5wb3VraWJzY21wamd2ZHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5NjU4NDEsImV4cCI6MTk5ODU0MTg0MX0.LjLMP0JW5Cj83eB-1rX2QLcUCkk_9T6y8oVUS1MBNHw";
+// Rutas relacionadas a los productos
+app.get('/apiv2/categorias', apiCategoriasController.getCategorias);
+app.get('/apiv2/categorias/:idCategoria', apiCategoriasController.show);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Rutas relacionadas a los pedidos
+app.get('/apiv2/pedidos/:idCliente', apiPedidosController.obtenerPedidosCliente);
+app.post('/apiv2/pedidos', apiPedidosController.crearPedido);
 
+// Rutas relacionadas a los clientes
+app.get('/apiv2/clientes', apiClientesController.obtenerClientes);
+app.get('/apiv2/clientes/:idCliente', apiClientesController.obtenerClientePorId);
+app.get('/apiv2/login', apiClientesController.loginDeCliente);
+app.post('/apiv2/crearCliente', apiClientesController.crearCliente);
+app.post('/apiv2/nuevaContrasenia', apiClientesController.cambiarContrasenia);
 
+// Iniciar el servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+});
